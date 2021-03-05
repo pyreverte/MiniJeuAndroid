c5b150f67c41b9c52a7c5f92b35106b99436a6c4
Pierre-Yves Reverte <me@pierreyves-reverte.com>
2021-03-05 14:50:42 +0100

    First Setup

---
+++ app/src/main/java/helloandroid/m2dl/minijeuandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package helloandroid.m2dl.minijeuandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
